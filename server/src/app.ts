da35668b674f9ae8c2b0c3620f7f812cf6cf2cc0
dovi stein <dovish@gmail.com>
2024-12-25 11:30:22 +0200

    cac

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 const corsOptions ={
-  origin:'*', 
+  origin:'http://localhost:5173', 
   credentials:true, 
   optionSuccessStatus:200
 }
